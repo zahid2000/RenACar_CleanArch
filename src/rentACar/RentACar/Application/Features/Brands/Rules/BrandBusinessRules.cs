417693c546e3c850849fd66a688ee6525f1f7189
zahid2000 <zahidmamedov07@gmail.com>
2023-09-14 15:33:54 +0400

    added business rules features

---
+++ src/rentACar/RentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -0,0 +1,24 @@
+﻿using Application.Features.Brands.Constants;
+using Application.Services.Repositories;
+using Core.Application.Rules;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+
+namespace Application.Features.Brands.Rules;
+
+public class BrandBusinessRules:BaseBusinessRules
+{
+    private readonly IBrandRepository  _brandRepository;
+
+    public BrandBusinessRules(IBrandRepository brandRepository)
+    {
+        _brandRepository = brandRepository;
+    }
+
+    public async Task BrandNameCannotBeDuplicatedWhenInserted(string name,CancellationToken cancellationToken=default)
+    {
+        bool brand = await _brandRepository.AnyAsync(b=>b.Name==name,cancellationToken:cancellationToken);
+
+        if (brand)
+            throw new BusinessException(BrandsMessages.BrandNameExists);
+    }
+}
